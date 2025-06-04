4c287ed5f56a6e522f01680e55241ac6ac502c4b
Nakatani Shuyo <nakatani.shuyo@gmail.com>
2025-06-04 22:26:03 +0900

    Add additional unit tests for game mechanics

--- tests/test_tricolore.py
+++ tests/test_tricolore.py
@@ -18,3 +18,36 @@ def test_availableplaces_start():
     for stone in (tricolore.RED, tricolore.BLUE):
         stones, whites = tricolore.availableplaces(board, stone)
         assert len(stones) + len(whites) > 0
+
+
+def test_pos_tuple_roundtrip():
+    for y in range(6):
+        for x in range(6):
+            pos = tricolore.tuple2pos((y, x))
+            assert tricolore.pos2tuple(pos) == (y, x)
+
+
+def test_putstone_flip():
+    board = tricolore.initboard()
+    move = tricolore.tuple2pos((1, 3))
+    tricolore.putstone(board, tricolore.RED, move)
+    (red, blue), blank = tricolore.score(board)
+    assert red == 3
+    assert blue == 1
+    assert blank == 31
+
+
+def test_putstoneW_flips_to_white():
+    board = tricolore.initboard()
+    board[tricolore.tuple2pos((2, 2))] = tricolore.RED
+    board[tricolore.tuple2pos((2, 3))] = tricolore.RED
+    board[tricolore.tuple2pos((2, 4))] = tricolore.W_RED
+    move = tricolore.tuple2pos((2, 1))
+    tricolore.putstoneW(board, tricolore.BLUE, move)
+    assert board[move] == tricolore.W_BLUE
+    assert board[tricolore.tuple2pos((2, 2))] == tricolore.W_RED
+    assert board[tricolore.tuple2pos((2, 3))] == tricolore.W_RED
+    (red, blue), blank = tricolore.score(board)
+    assert red == 1
+    assert blue == 1
+    assert blank == 30
